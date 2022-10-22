8e281db01ee6249ab34047d8a85fadad99ba166b
AB0s <93539709+AB0s@users.noreply.github.com>
2022-10-23 00:08:00 +0600

    New List View

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:codedesign/constants.dart';
+import 'package:codedesign/model/course.dart';
 import 'package:flutter/material.dart';
 import 'components/lists/recent_course.dart';
 import 'components/home_screen_navbar.dart';
@@ -39,6 +40,20 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
                 RecentCourseList(),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 20, right: 20, top: 25, bottom: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text(
+                        "Explore",
+                        style: kTitle1Style,
+                      )
+                    ],
+                  ),
+                ),
+                ExploreCourseList(),
               ],
             ),
           ),
@@ -48,5 +63,81 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class ExploreCourseList extends StatelessWidget {
+  const ExploreCourseList({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 120,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        itemCount: exploreCourses.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.only(left: index==0? 20.0 : 0.0),
+            child: ExploreCourseCard(course: exploreCourses[index]),
+          );
+        },
+      ),
+    );
+  }
+}
 
+class ExploreCourseCard extends StatelessWidget {
+  ExploreCourseCard({required this.course});
+  final Course course;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 20.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(41.0),
+        child: Container(
+          height: 120,
+          width: 280,
+          decoration: BoxDecoration(
+            gradient: course.background,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 32.0),
+            child: Row(
+              children: [
+                Expanded(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      course.courseSubtitle,
+                      style: kCardSubtitleStyle,
+                    ),
+                    const SizedBox(
+                      height: 6,
+                    ),
+                    Text(
+                      course.courseTitle,
+                      style: kCardTitleStyle,
+                    )
+                  ],
+                )),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Image.asset(
+                      'asset/illustrations/${course.illustration}',
+                      fit: BoxFit.cover,
+                      height: 100,
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
