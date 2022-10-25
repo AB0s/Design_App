199e86372ddbefbe0413b3e5a7fe394d1e0ba4dd
AB0s <93539709+AB0s@users.noreply.github.com>
2022-10-25 13:55:12 +0600

    new package slider

---
+++ lib/screens/continue_watching_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+
+import '../constants.dart';
+
+class ContinueWatchingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SlidingUpPanel(
+      backdropEnabled: true,
+      borderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(34),
+      ),
+      color: kCardPopupBackgroundColor,
+      boxShadow: const [
+        BoxShadow(color: kShadowColor, offset: Offset(0, -12), blurRadius: 32),
+      ],
+      minHeight: 85,
+      maxHeight: MediaQuery.of(context).size.height * 0.75,
+      panel: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: Padding(
+              padding: EdgeInsets.only(top: 12, bottom: 16),
+              child: Container(
+                width: 42,
+                height: 4,
+                decoration: BoxDecoration(
+                    color: Color(0xFFC5CDB6),
+                    borderRadius: BorderRadius.circular(2)),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 32),
+            child: Text(
+              "Continue watching",
+              style: kTitle2Style,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 32),
+            child: Text(
+              "Certificates",
+              style: kTitle2Style,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,12 @@
 import 'package:codedesign/screens/sidebar_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:codedesign/constants.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 import '../components/home_screen_navbar.dart';
 import '../components/lists/explore_course_list.dart';
 import '../components/lists/recent_course.dart';
+import 'continue_watching_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -98,6 +100,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                 ],
               ),
             ),
+            ContinueWatchingScreen(),
             IgnorePointer(
               ignoring: sidebarHidden,
               child: Stack(
@@ -134,3 +137,4 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
     );
   }
 }
+
