73d71394446212f50b8ef66ce0cf2e563b2d1030
AB0s <93539709+AB0s@users.noreply.github.com>
2022-10-25 14:32:05 +0600

    Added Cards

--- lib/screens/continue_watching_screen.dart
+++ lib/screens/continue_watching_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 import '../constants.dart';
+import '../model/course.dart';
 
 class ContinueWatchingScreen extends StatelessWidget {
   @override
@@ -39,6 +40,7 @@ class ContinueWatchingScreen extends StatelessWidget {
               style: kTitle2Style,
             ),
           ),
+          ContinueWatchingList(),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 32),
             child: Text(
@@ -46,6 +48,161 @@ class ContinueWatchingScreen extends StatelessWidget {
               style: kTitle2Style,
             ),
           ),
+        ],
+      ),
+    );
+  }
+}
+
+class ContinueWatchingList extends StatefulWidget {
+  const ContinueWatchingList({Key? key}) : super(key: key);
+
+  @override
+  State<ContinueWatchingList> createState() => _ContinueWatchingListState();
+}
+
+class _ContinueWatchingListState extends State<ContinueWatchingList> {
+  List<Container> indicators = [];
+  int currentPage = 0;
+  Widget updateIndicators() {
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: continueWatchingCourses.map((course) {
+          var index = continueWatchingCourses.indexOf(course);
+          return Container(
+            height: 7,
+            width: 7,
+            margin: const EdgeInsets.symmetric(horizontal: 6),
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: currentPage == index
+                    ? Color(0xFF0971FE)
+                    : Color(0xFFA6AEBD)),
+          );
+        }).toList());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 200,
+          width: double.infinity,
+          child: PageView.builder(
+            itemBuilder: (context, index) {
+              return ContinueWatchingCard(
+                course: continueWatchingCourses[index],
+              );
+            },
+            itemCount: continueWatchingCourses.length,
+            onPageChanged: (index){
+              setState((){
+                currentPage=index;
+              });
+            },
+            controller:PageController(initialPage: 0,viewportFraction: 0.75),
+          ),
+        ),
+        updateIndicators(),
+      ],
+    );
+  }
+}
+
+class ContinueWatchingCard extends StatelessWidget {
+  ContinueWatchingCard({required this.course});
+  Course course;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.topCenter,
+      child: Stack(
+        alignment: Alignment.topRight,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 20, right: 20),
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: course.background,
+                borderRadius: BorderRadius.circular(41),
+                boxShadow: [
+                  BoxShadow(
+                    color: course.background.colors[0].withOpacity(0.3),
+                    offset: const Offset(0, 20),
+                    blurRadius: 30,
+                  ),
+                  BoxShadow(
+                    color: course.background.colors[1].withOpacity(0.3),
+                    offset: const Offset(0, 20),
+                    blurRadius: 30,
+                  ),
+                ],
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(41),
+                child: Container(
+                  height: 140,
+                  width: 255,
+                  child: Stack(
+                    children: [
+                      Row(
+                        children: [
+                          Spacer(),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Image.asset(
+                                'asset/illustrations/${course.illustration}',
+                                fit: BoxFit.cover,
+                                height: 110,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(32),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              course.courseSubtitle,
+                              style: kCardSubtitleStyle,
+                            ),
+                            const SizedBox(
+                              height: 6,
+                            ),
+                            Text(
+                              course.courseTitle,
+                              style: kCardTitleStyle,
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            width: 60,
+            height: 60,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(18),
+              boxShadow: const [
+                BoxShadow(
+                    color: kShadowColor, offset: Offset(0, 4), blurRadius: 16),
+              ],
+            ),
+            padding: const EdgeInsets.only(
+                top: 12.5, bottom: 13, right: 14.5, left: 20.5),
+            child: Image.asset('asset/icons/icon-play.png'),
+          )
         ],
       ),
     );
