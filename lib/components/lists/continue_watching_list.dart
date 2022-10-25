18504832798d01a2769d8fb15f288a5b1ce6cb58
AB0s <93539709+AB0s@users.noreply.github.com>
2022-10-25 15:44:41 +0600

    Dynamic Widgets with Random

---
+++ lib/components/lists/continue_watching_list.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import '../../model/course.dart';
+import '../cards/continue_watching_card.dart';
+class ContinueWatchingList extends StatefulWidget {
+  const ContinueWatchingList({Key? key}) : super(key: key);
+
+  @override
+  State<ContinueWatchingList> createState() => _ContinueWatchingListState();
+}
+
+class _ContinueWatchingListState extends State<ContinueWatchingList> {
+  List<Container> indicators = [];
+  int currentPage = 0;
+  Widget updateIndicators() {
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: continueWatchingCourses.map((course) {
+          var index = continueWatchingCourses.indexOf(course);
+          return Container(
+            height: 7,
+            width: 7,
+            margin: const EdgeInsets.symmetric(horizontal: 6),
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: currentPage == index
+                    ? Color(0xFF0971FE)
+                    : Color(0xFFA6AEBD)),
+          );
+        }).toList());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 200,
+          width: double.infinity,
+          child: PageView.builder(
+            itemBuilder: (context, index) {
+              return ContinueWatchingCard(
+                course: continueWatchingCourses[index],
+              );
+            },
+            itemCount: continueWatchingCourses.length,
+            onPageChanged: (index) {
+              setState(() {
+                currentPage = index;
+              });
+            },
+            controller: PageController(initialPage: 0, viewportFraction: 0.75),
+          ),
+        ),
+        updateIndicators(),
+      ],
+    );
+  }
+}
